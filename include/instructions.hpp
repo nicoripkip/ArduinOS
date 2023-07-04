462c9da8a93c945a6c1823ae4307dbed1cef5d54
Nikoripkip <nicovanommen.nvo@gmail.com>
2023-07-04 21:39:42 +0200

    Add instruction layout

---
+++ include/instructions.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <Arduino.h>
+
+
+#ifndef __TINBES03__ARDUINOS__INSTRUCTIONS__
+#define __TINBES03__ARDUINOS__INSTRUCTIONS__
+
+
+void execute(byte instruction);
+
+
+#endif
